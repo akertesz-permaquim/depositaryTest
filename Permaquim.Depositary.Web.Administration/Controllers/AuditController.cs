0624dd73c1906dc18f15bf6b0e5b48ca19ea8311
akertesz-permaquim <107046032+akertesz-permaquim@users.noreply.github.com>
2022-09-05 11:08:59 -0300

    Audit controller added

---
+++ Permaquim.Depositary.Web.Administration/Controllers/AuditController.cs
@@ -0,0 +1,44 @@
+﻿using Permaquim.Depositary.Web.Administration.Entities;
+using Permaquim.Depositary.Web.Administration.Managers;
+using System.Diagnostics;
+using static Permaquim.Depositary.Web.Administration.Entities.AuditEntities;
+
+namespace Permaquim.Depositary.UI.Desktop.Controllers
+{
+    internal static class AuditController
+    {
+        public static void Log(LogTypeEnum logType, string description, string detail)
+        {
+            try
+            {
+                StackTrace stackTrace = new StackTrace(); //Para obtener el nombre del metodo que lo llamo
+
+                Permaquim.Depositary.Business.Tables.Auditoria.Log entities = new();
+                Permaquim.Depositary.Entities.Tables.Auditoria.Log entity = new()
+                {
+                    AplicacionId = AuditEntities.APPLICATION_ID,
+                    Fecha = DateTime.Now,
+                    Metodo = stackTrace.GetFrame(1).GetMethod().Name,
+                    Modulo = stackTrace.GetFrame(1).GetMethod().Module.Name,
+                    TipoId = (long)logType,
+                    Descripcion = description,
+                    Detalle = detail,
+                    UsuarioId = -1
+
+                };
+
+                entities.Add(entity);
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        public static void Log(Exception exception)
+        {
+            Log(LogTypeEnum.Exception, exception.Message, exception.StackTrace);
+        }
+
+    }
+}
