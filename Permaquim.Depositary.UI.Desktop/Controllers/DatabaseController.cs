72ca619a8b2a9ea610bb9f8bff5e9099572ffe5a
cdcespon <cdcespon@gmail.com>
2022-11-04 14:47:38 -0300

    Licencia

--- Permaquim.Depositary.UI.Desktop/Controllers/DatabaseController.cs
+++ Permaquim.Depositary.UI.Desktop/Controllers/DatabaseController.cs
@@ -1759,6 +1759,7 @@ namespace Permaquim.Depositary.UI.Desktop.Controllers
 
                 newClosing = closingEntities.Add(new Permaquim.Depositario.Entities.Tables.Operacion.CierreDiario()
                 {
+                    Fecha = DateTime.Now,
                     FechaCreacion = DateTime.Now,
                     FechaModificacion = null,
                     Nombre = CIERRE_DIARIO,
