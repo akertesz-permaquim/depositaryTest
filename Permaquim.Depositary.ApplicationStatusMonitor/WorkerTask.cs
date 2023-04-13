85e677343895949114c7a27d5a7ecebe6badba91
akertesz-permaquim <107046032+akertesz-permaquim@users.noreply.github.com>
2023-04-13 10:23:19 -0300

    App Status monitor update
    Update:
    -Manejo de paths no validos
    -Manejo de programas que crashean
    -Logs

--- Permaquim.Depositary.ApplicationStatusMonitor/WorkerTask.cs
+++ Permaquim.Depositary.ApplicationStatusMonitor/WorkerTask.cs
@@ -2,6 +2,51 @@
 {
     internal class WorkerTask
     {
+<<<<<<< Updated upstream
+=======
+        public bool Failing { get; set; }
+        private bool _fileCheck = false;
+        public bool FileExistCheck { get
+            {
+                if (!_fileCheck)
+                {
+                    _fileCheck = true;
+                    return false;
+                }
+                return _fileCheck;
+            }
+        }
+        public bool FileExists => !string.IsNullOrEmpty(Target) && File.Exists(Target);
+        public SafeProcessHandle ProcessHandle { private get; set; }
+
+        public Process Process { get; set; }
+
+        public int ExecutionOrder { get; set; }
+
+        public string Target { get; set; }
+
+        public int SleepTime { get; set; }
+
+        public int ProcessId { get; set; }
+
+        public string ProcessName
+        {
+            get
+            {
+                try
+                {
+                    return Path.GetFileName(this.Target);
+                }
+                catch
+                {
+                    return this.Target;
+                }
+            }
+        }
+
+        public WorkerTask.WorkerTaskTypeEnum WorkerTaskType { get; set; }
+
+>>>>>>> Stashed changes
         public enum WorkerTaskTypeEnum
         {
             None,
